cd8259ec39cc0b85ab475451a597c280af3fbd2f
Sidh <sidharta.rezende@gmail.com>
2018-12-01 17:39:48 -0200

    initial

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/GatewayViviPay';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/OAUTH';
 } else if (env === 'test') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/GatewayViviPay'
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/OAUTH'
 }
 
 module.exports = {
